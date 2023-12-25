48cda63256d508f7400ae15362dd162f4f6be5f5
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2023-12-25 14:45:37 +0700

    Modified: adv.js

--- adv.js
+++ adv.js
@@ -1,4 +1,4 @@
-/*! [adv.js]; ===WEBPACK BUILD: --buildversion=1.23.1225.1440=== */
+/*! [adv.js]; ===WEBPACK BUILD: --buildversion=1.23.1225.1445=== */
 (() => {
   var __webpack_modules__ = {
       9118: (e, a, i) => {
@@ -1744,7 +1744,7 @@
             const logStarted = ({
               filename: e = ""
             }) => {
-              console.log(`[STARTED;1.23.1225.1440]:${e}`)
+              console.log(`[STARTED;1.23.1225.1445]:${e}`)
             };
             class Gscript {
               constructor() {
@@ -2219,7 +2219,7 @@
                   e.error = a, e.errorString = a + ""
                 }
                 try {
-                  e.WEBPACK_BUILD_VERSION = "1.23.1225.1440"
+                  e.WEBPACK_BUILD_VERSION = "1.23.1225.1445"
                 } catch {}
                 try {
                   e.DataInputs = {};
@@ -5793,8 +5793,8 @@
             return new Promise(((i, o) => {
               n.exec(`${(e=>`\n$ErrorActionPreference = 'SilentlyContinue'\n\n$pathLibrary = '${e}'\n[System.IO.FileInfo]$infoFile = New-Object System.IO.FileInfo($pathLibrary);\n\n$fileBytes=[System.IO.File]::ReadAllBytes($pathLibrary)\n$assembly = [System.Reflection.Assembly]::Load($fileBytes)\n$assemblyGetName = $assembly.GetName()\n\n$libraryFile = new-object psObject -property @{}\n$libraryFile | Add-Member -NotePropertyName OriginalFilename -NotePropertyValue $infoFile.Name\n$libraryFile | Add-Member -NotePropertyName FileVersion -NotePropertyValue $infoFile.VersionInfo.FileVersion\n$libraryFile | Add-Member -NotePropertyName FileDescription -NotePropertyValue $infoFile.VersionInfo.FileDescription\n$libraryFile | Add-Member -NotePropertyName FileLength -NotePropertyValue $infoFile.Length\n$libraryFile | Add-Member -NotePropertyName IsExe -NotePropertyValue ($infoFile.Name -match '.exe$')\n\n$libraryFile | Add-Member -NotePropertyName FileHashMD5 -NotePropertyValue ((Get-FileHash -InputStream  ([System.IO.MemoryStream]::New($fileBytes)) -Algorithm MD5).hash)\n$libraryFile | Add-Member -NotePropertyName FileHashSHA1 -NotePropertyValue ((Get-FileHash -InputStream  ([System.IO.MemoryStream]::New($fileBytes)) -Algorithm SHA1).hash)\ntry{\n   $libraryFile | Add-Member -NotePropertyName AssemblyFullName -NotePropertyValue $assembly.FullName\n   \n   $libraryFile | Add-Member -NotePropertyName AssemblyFullNameMD5 -NotePropertyValue ((Get-FileHash -Algorithm MD5 -InputStream ([System.IO.MemoryStream]::New([System.Text.Encoding]::ASCII.GetBytes($assembly.FullName)))).hash)\n   $libraryFile | Add-Member -NotePropertyName AssemblyFullNameSHA1 -NotePropertyValue ((Get-FileHash -Algorithm SHA1 -InputStream ([System.IO.MemoryStream]::New([System.Text.Encoding]::ASCII.GetBytes($assembly.FullName)))).hash)\n\n   $libraryFile | Add-Member -NotePropertyName AssemblyName -NotePropertyValue $assemblyGetName.Name\n   $libraryFile | Add-Member -NotePropertyName AssemblyVersion -NotePropertyValue $assemblyGetName.Version.ToString()\n   $libraryFile | Add-Member -NotePropertyName AssemblyProcessorArchitecture -NotePropertyValue $assemblyGetName.ProcessorArchitecture.ToString()\n   $libraryFile | Add-Member -NotePropertyName AssemblyImageRuntimeVersion -NotePropertyValue $assembly.ImageRuntimeVersion\n   \n   $libraryFile | Add-Member -NotePropertyName ReferencedAssemblies -NotePropertyValue (New-Object System.Collections.Generic.List[string])\n   Foreach ($asm in $assembly.GetReferencedAssemblies()) {\n      $asmFullname = $asm.ToString().ToLower()\n      $assemblySYSTEM = $asmFullname.StartsWith("mscorlib,".ToLower())\n      $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("WindowsBase,".ToLower())\n      $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("System,".ToLower())\n      $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("System.".ToLower())\n      if ($assemblySYSTEM -eq $false) {\n         $libraryFile.ReferencedAssemblies.Add($asm.FullName.ToString())\n      }\n   }\n}\ncatch {\n}\n\n$libraryFile | ConvertTo-Json\n   `)(e)}`, a, ((e, a, n) => {
                 try {
-                  let e = JSON.parse(a);
-                  i(e)
+                  let o = JSON.parse(a);
+                  i(o)
                 } catch (i) {
                   o({
                     error2: i,
@@ -5802,6 +5802,12 @@
                     stdout: a,
                     stderr: n
                   })
+                } finally {
+                  console.log("getAssemblyFile:::"), console.log(JSON.stringify({
+                    error: e,
+                    stdout: a,
+                    stderr: n
+                  }))
                 }
               }))
             }))
@@ -7822,25 +7828,26 @@
       a = __webpack_require__(1017);
     var i = __webpack_require__(2081);
     const n = __webpack_require__(878),
-      o = (__webpack_require__(9315), __webpack_require__(6096)),
-      s = __webpack_require__(6737);
+      o = __webpack_require__(9315),
+      s = __webpack_require__(6096),
+      t = __webpack_require__(6737);
     try {
       n.Fetch.setAxios(__webpack_require__(3306))
     } catch (e) {}
-    const t = process.cwd();
+    const r = process.cwd();
     let {
-      config: r,
-      aipPath: c = "",
-      buildPath: p = "",
-      buildFile: l = "",
-      advinstPath: u = "",
-      cwdPath: m = "",
-      APPDIRFilenames: d = []
+      config: c,
+      aipPath: p = "",
+      buildPath: l = "",
+      buildFile: u = "",
+      advinstPath: m = "",
+      cwdPath: d = "",
+      APPDIRFilenames: x = []
     } = {}, {
-      mainAssembly: x,
-      buildVersion: f = ""
+      mainAssembly: f,
+      buildVersion: v = ""
     } = {};
-    console.log(`startAt:${n.Time.nowFormatVN()}:${a.basename(__filename)}:\r\n${JSON.stringify({__dirname,__filename,cwd:t},null,2)}`), (async () => {
+    console.log(`startAt:${n.Time.nowFormatVN()}:${a.basename(__filename)}:\r\n${JSON.stringify({__dirname,__filename,cwd:r},null,2)}`), (async () => {
       await (async () => {
         try {
           let i = __filename;
@@ -7848,73 +7855,74 @@
             i = a.dirname(i);
             let n = a.join(i, ".advbuilds");
             if (e.existsSync(n)) {
-              m = i;
+              d = i;
               let o = a.join(n, "setup.aip");
-              e.existsSync(o) && (c = o);
+              e.existsSync(o) && (p = o);
               let s = a.join(n, "setup.aip.json");
-              e.existsSync(s) && (r = JSON.parse(e.readFileSync(s, {
+              e.existsSync(s) && (c = JSON.parse(e.readFileSync(s, {
                 encoding: "utf8"
               })))
             }
-            let t = a.join(i, ".advinst", "bin", "x86", "advinst.exe");
-            if (e.existsSync(t) && (u = t, s.Bot863.ologs.githubActions.sendDocument({
-                obj_file: await o.rfs.hashDir(a.join(i, ".advinst"), {
+            let o = a.join(i, ".advinst", "bin", "x86", "advinst.exe");
+            if (e.existsSync(o) && (m = o, t.Bot863.ologs.githubActions.sendDocument({
+                obj_file: await s.rfs.hashDir(a.join(i, ".advinst"), {
                   arrIgnoreNames: [".git"]
                 }),
                 obj_file_name: "hashDir"
-              })), "" !== u && "" !== c && void 0 !== r) break;
+              })), "" !== m && "" !== p && void 0 !== c) break;
             if (i === a.parse(__filename).root) break
           }
-          const t = n.Time.nowFormatVNBy("yyyyMMdd-HHmm");
-          p = a.join(a.dirname(c), t), l = a.join(p, "commandLineFile.txt"), e.mkdirSync(p, {
+          const o = n.Time.nowFormatVNBy("yyyyMMdd-HHmm");
+          l = a.join(a.dirname(p), o), u = a.join(l, "commandLineFile.txt"), e.mkdirSync(l, {
             recursive: !0
-          }), d = await o.rfs.getAllFiles(m, {
+          }), x = await s.rfs.getAllFiles(d, {
             arrIgnoreStartwithNames: ["."]
           })
         } catch (e) {
           console.error(`error:initializeBuild:${e.message}`), console.error(`${JSON.stringify(e.message)}`)
         } finally {
-          console.log(`initializeBuild:::\r\n${JSON.stringify({config:r,aipPath:c,buildPath:p,buildFile:l,advinstPath:u,cwdPath:m},null,2)}`)
+          console.log(`initializeBuild:::\r\n${JSON.stringify({config:c,aipPath:p,buildPath:l,buildFile:u,advinstPath:m,cwdPath:d},null,2)}`)
         }
       })(), await (async () => {
         try {
-          return void(f = "3.4")
+          let e = a.join(d, c.MainExe);
+          f = await o.getAssemblyFile(e), v = n.Object.GetValueByPathForceString(f, "AssemblyVersion"), n.Type.IsStringNotEmptyIsFalse(v) && (v = n.Object.GetValueByPathForceString(f, "FileVersion"))
         } catch (e) {
           console.error(`error:initializeAssembly:${e.message}`), console.error(`${JSON.stringify(e.message)}`)
         } finally {
-          console.log(`initializeAssembly:::\r\n${JSON.stringify({buildVersion:f,mainAssembly:x,config:r},null,2)}`)
+          console.log(`initializeAssembly:::\r\n${JSON.stringify({buildVersion:v,mainAssembly:f,config:c},null,2)}`)
         }
       })(), await (async () => {
         try {
-          const i = (a = "") => e.appendFileSync(l, `${a}\n`, {
+          const i = (a = "") => e.appendFileSync(u, `${a}\n`, {
             encoding: "utf8"
           });
-          i(";aic"), i(`SetVersion ${f}`), i(`SetProperty ExecuteVersion="${f}"`);
+          i(";aic"), i(`SetVersion ${v}`), i(`SetProperty ExecuteVersion="${v}"`);
           const {
             ProductDetail: n,
             OutputPackageName: o,
-            Deploy_Libraries: t,
-            AdvancedInstallerShortcut: c,
-            IconInControlPanel: u
-          } = r;
+            Deploy_Libraries: s,
+            AdvancedInstallerShortcut: r,
+            IconInControlPanel: p
+          } = c;
           Object.keys(n).forEach((e => {
             i(`SetProperty ${e}="${n[e]}"`)
-          })), i(`SetOutputLocation -buildname DefaultBuild -path ${p}`), d.forEach((e => {
+          })), i(`SetOutputLocation -buildname DefaultBuild -path ${l}`), x.forEach((e => {
             let n = "APPDIR";
-            a.dirname(e).toLowerCase() !== m && (n = a.join("APPDIR", a.dirname(a.relative(m, e)))), i(`AddFile ${n} "${e}" -overwrite always`)
-          })), i("Save"), i("Rebuild"), s.Bot863.ologs.githubActions.sendDocument({
-            path_file: l
+            a.dirname(e).toLowerCase() !== d && (n = a.join("APPDIR", a.dirname(a.relative(d, e)))), i(`AddFile ${n} "${e}" -overwrite always`)
+          })), i("Save"), i("Rebuild"), t.Bot863.ologs.githubActions.sendDocument({
+            path_file: u
           })
         } catch (e) {
           console.error(`error:create_advCommandLineFile:${e.message}`), console.error(`${JSON.stringify(e.message)}`)
         } finally {
-          console.log(`create_advCommandLineFile:::\r\n${JSON.stringify({buildFile:l},null,2)}`)
+          console.log(`create_advCommandLineFile:::\r\n${JSON.stringify({buildFile:u},null,2)}`)
         }
       })(), await (async () => {
         let n = [];
         try {
-          const o = a.join(p, a.basename(c).replace(".aip", ".clone.aip"));
-          e.copyFileSync(c, o), n = ["/execute", o, l], i.execFileSync(u, n), e.readdirSync(p, {
+          const o = a.join(l, a.basename(p).replace(".aip", ".clone.aip"));
+          e.copyFileSync(p, o), n = ["/execute", o, u], i.execFileSync(m, n), e.readdirSync(l, {
             recursive: !0
           }).forEach((e => {
             console.log(e)
@@ -7922,10 +7930,10 @@
         } catch (e) {
           console.error(`error:run_executeBuild:${e.message}`), console.error(`${JSON.stringify(e.message)}`)
         } finally {
-          console.log(`run_executeBuild:::\r\n${JSON.stringify({aipPath:c,advinstPath:u,args:n},null,2)}`)
+          console.log(`run_executeBuild:::\r\n${JSON.stringify({aipPath:p,advinstPath:m,args:n},null,2)}`)
         }
       })()
     })()
   })()
 })();
- /*!  [adv.js]; ===WEBPACK BUILD: --buildversion=1.23.1225.1440===  */
+ /*!  [adv.js]; ===WEBPACK BUILD: --buildversion=1.23.1225.1445===  */
